20ec837493ae804c7d4ecc84d7a883542db19db5
gjamade <trangiangkt@gmail.com>
2022-03-21 20:49:29 +0700

    add "app"

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,7 +1,7 @@
 import express from "express";
-let configViewengine = () => {
+let configViewEngine = (app) => {
     app.use(express.static("./src/public"));
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
-module.exports = configViewengine;
+module.exports = configViewEngine;
